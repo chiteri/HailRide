fabd338a67e0956110deb3d19ba1fbc6d6b0baac
Martin Chiteri <martin.chiteri@gmail.com>
2020-02-11 11:19:10 -0700

    Cleaned up routes, grouped them into a single file

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const express   = require("express"),
 	 User		= require("./models/user");
 ;
 
+// Require routes separately
+const authRoutes = require("./routes/index");
+
 var app = express();
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended:true}));
@@ -29,76 +32,7 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-// A middleware function to include a new user's data to each page
-app.use((req, res, next) => {
-	res.locals.currentUser = req.user;
-	next();
-});
-
-// The landing page
-app.get("/", (req, res) => {
-	res.render("login");
-});
-
-// =============
-//  AUTH ROUTES
-// =============
-// The user registration page
-app.get("/register", (req, res) => {
-	res.render("register");
-});
-
-// Handle sign up logic
-app.post("/register", (req, res) => {
-	var newUser = new User({username: req.body.username});
-	User.register(newUser, req.body.password, (err, user) => {
-		if(err) {
-			console.log(err);
-			return res.render("register");
-		}
-		// Else authenticate the new user
-		passport.authenticate("local")(req, res, () => {
-			res.redirect("/home");
-		});
-	});
-});
-
-// Show login form 
-app.get("/login", (req, res) => {
-	res.render("login");
-});
-
-// Handling login logic
-app.post("/login", passport.authenticate("local", {
-		successRedirect: "/home",
-		failureRedirect: "/login"
-	}), (req, res) => {
-});
-
-// The landing page for a logged in user
-app.get("/home", isLoggedIn, (req, res) => {
-	res.render("home");
-});
-
-// Logout logic
-app.get("/logout", (req, res) => {
-	req.logout();
-	res.redirect("/login");
-});
-
-// Middleware to confirm if a user is logged in
-function isLoggedIn(req, res, next) {
-	if (req.isAuthenticated()) {
-		return next();
-	}
-	res.redirect("/login");
-}
-
-// * Catch-all route matcher (Everything else)
-app.get("*", (req, res) => {
-	res.send("Howdy? What are you doing with your life?");
-});
-
+app.use("/", authRoutes);
 
 // Tell Express to listen for requests (start server)
 app.listen(3000, () => {
---
+++ routes/index.js
@@ -0,0 +1,77 @@
+const express  = require("express"), 
+      passport = require("passport"),
+	  User     = require("../models/user");
+	  
+var router = express.Router();
+
+// A middleware function to include a new user's data to each page
+router.use((req, res, next) => {
+	res.locals.currentUser = req.user;
+	next();
+});
+
+// The landing page
+router.get("/", (req, res) => {
+	res.render("login");
+});
+
+// =============
+//  AUTH ROUTES
+// =============
+// The user registration page
+router.get("/register", (req, res) => {
+	res.render("register");
+});
+
+// Handle sign up logic
+router.post("/register", (req, res) => {
+	var newUser = new User({username: req.body.username});
+	User.register(newUser, req.body.password, (err, user) => {
+		if(err) {
+			console.log(err);
+			return res.render("register");
+		}
+		// Else authenticate the new user
+		passport.authenticate("local")(req, res, () => {
+			res.redirect("/home");
+		});
+	});
+});
+
+// Show login form 
+router.get("/login", (req, res) => {
+	res.render("login");
+});
+
+// Handling login logic
+router.post("/login", passport.authenticate("local", {
+		successRedirect: "/home",
+		failureRedirect: "/login"
+	}), (req, res) => {
+});
+
+// The landing page for a logged in user
+router.get("/home", isLoggedIn, (req, res) => {
+	res.render("home");
+});
+
+// Logout logic
+router.get("/logout", (req, res) => {
+	req.logout();
+	res.redirect("/login");
+});
+
+// Middleware to confirm if a user is logged in
+function isLoggedIn(req, res, next) {
+	if (req.isAuthenticated()) {
+		return next();
+	}
+	res.redirect("/login");
+}
+
+// * Catch-all route matcher (Everything else)
+router.get("*", (req, res) => {
+	res.send("Howdy? What are you doing with your life?");
+});
+
+module.exports = router;
